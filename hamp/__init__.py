70451d76e901a7cc948184e290c7b41b47943904
Stefan Sandstrom <stefan.sandstrom@pacetarc.com>
2023-08-06 20:42:49 +0200

    Export stuff at top level

---
+++ hamp/__init__.py
@@ -0,0 +1,21 @@
+from ._hwtypes import (
+    uint,
+    sint,
+    clock,
+    reset,
+    async_reset,
+    sync_reset,
+    INPUT,
+    OUTPUT,
+)
+from ._module import (
+    module,
+    input,
+    output,
+    wire,
+    register,
+    attribute,
+    unique,
+    instance,
+    modules,
+)
